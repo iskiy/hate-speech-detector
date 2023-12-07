7bc342891e51489e077d5e9f8acbb95feab97a93
iskiy <vasyarosada@gmail.com>
2023-12-07 23:24:29 +0200

    fix: githubworkflow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,16 @@ import pytest
 
 
 def pytest_addoption(parser):
+    """Add option to specify dataset location when executing tests from CLI.
+    Ex: pytest --dataset-loc=checkpoints/data.csv tests/data --verbose --disable-warnings
+    """
     parser.addoption(
         "--dataset-loc", action="store", default=None, help="Dataset location."
     )
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def df(request):
     dataset_loc = request.config.getoption("--dataset-loc")
-    df = ge.dataset.PandasDataset(pd.read_csv(dataset_loc))
+    df = dat
     return df
