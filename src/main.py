48e1b2b11987ca161c0649b340d6ed6f64dc5144
Andrii Chaliuk <andrii.chaliuk@gmail.com>
2023-12-07 20:31:49 +0200

    load, save and tests for dataset and output data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import hydra
 # import numpy as np
-# import pandas as pd
+import pandas as pd
 from omegaconf import DictConfig
 
 # from sklearn.ensemble import RandomForestClassifier
@@ -9,6 +9,17 @@ from omegaconf import DictConfig
 # from sklearn.model_selection import train_test_split
 # from sklearn.multioutput import MultiOutputClassifier
 # import filter_text
+
+
+def load_data(file):
+    df = pd.read_csv(file)
+
+    df.drop_duplicates(inplace=True)
+    return df
+
+
+def save_data(file_name, df):
+    df.to_csv(file_name, index=False)
 
 
 @hydra.main(version_base=None, config_path="./../configs", config_name="config")
---
+++ tests/code/test_dataset.py
@@ -0,0 +1,43 @@
+def test_dataset(df):
+    """Test dataset quality and integrity."""
+    # schema adherence
+    column_list = ["id", "comment_text", "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+    df.expect_table_columns_to_match_ordered_list(column_list=column_list)
+    # expected labels
+    labels = [0, 1]
+    df.expect_column_values_to_be_in_set(column="toxic", value_set=labels)
+    df.expect_column_values_to_be_in_set(column="severe_toxic", value_set=labels)
+    df.expect_column_values_to_be_in_set(column="obscene", value_set=labels)
+    df.expect_column_values_to_be_in_set(column="threat", value_set=labels)
+    df.expect_column_values_to_be_in_set(column="insult", value_set=labels)
+    df.expect_column_values_to_be_in_set(column="identity_hate", value_set=labels)
+
+    # missing values
+    df.expect_column_values_to_not_be_null(column="id")
+    df.expect_column_values_to_not_be_null(column="comment_text")
+    df.expect_column_values_to_not_be_null(column="toxic")
+    df.expect_column_values_to_not_be_null(column="severe_toxic")
+    df.expect_column_values_to_not_be_null(column="obscene")
+    df.expect_column_values_to_not_be_null(column="threat")
+    df.expect_column_values_to_not_be_null(column="insult")
+    df.expect_column_values_to_not_be_null(column="identity_hate")
+
+    # unique values
+    df.expect_column_values_to_be_unique(column="id")
+    df.expect_column_values_to_be_unique(column="comment_text")
+
+    # type adherence
+    df.expect_column_values_to_be_of_type(column="comment_text", type_="str")
+    df.expect_column_values_to_be_of_type(column="toxic", type_="int")
+    df.expect_column_values_to_be_of_type(column="severe_toxic", type_="int")
+    df.expect_column_values_to_be_of_type(column="obscene", type_="int")
+    df.expect_column_values_to_be_of_type(column="threat", type_="int")
+    df.expect_column_values_to_be_of_type(column="insult", type_="int")
+    df.expect_column_values_to_be_of_type(column="identity_hate", type_="int")
+
+    # Expectation suite
+    expectation_suite = df.get_expectation_suite(discard_failed_expectations=False)
+    results = df.validate(
+        expectation_suite=expectation_suite, only_return_failures=True
+    ).to_json_dict()
+    assert results["success"]
